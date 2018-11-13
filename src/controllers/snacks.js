5d3268590fa318b41d97cc20adfd5bb8a453deb9
Sarah Lee <sarahselee223@gmail.com>
2018-11-12 23:32:46 -0800

    add gitignore and error handler

--- src/controllers/snacks.js
+++ src/controllers/snacks.js
@@ -9,19 +9,20 @@ function readAll (req, res, next){
 
 function readOne (req, res, next){
     const snack = snackModel.getOne(req.params.snackid)
-    
+    if(snack.error) return next({status: 404, message: snack.error })
     res.send(snack)
 }
 
 function createOne (req, res, next){
     const newSnack = snackModel.createOne(req.body.name, req.body.available, req.body.rate)
-    if(newSnack.error) return next ( {status:400, message: newSnack} )
+    if(newSnack.error) return next ( {status:400, message: newSnack.error} )
     res.send(newSnack)
 }
 
 function editOne (req, res, next){
     const id = req.params.snackid
     const snack = snackModel.editOne(id, req.body.name, req.body.available, req.body.rate)
+    if(snack.error) return next({status: 404, message: snack.error })
 
     res.send(snack)
 }
@@ -29,6 +30,7 @@ function editOne (req, res, next){
 function deleteOne (req, res, next){
     const id = req.params.snackid
     const deleteSnack = snackModel.deleteOne(id)
+    if(deleteSnack.error) return next({status: 404, message: deleteSnack.error })
 
     res.send(deleteSnack)
 }
